819e5c6d81a5f9353e0ac98877f2d3c2095dec11
Carlota Bindner <cev.bindner@gmail.com>
2018-03-21 14:41:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,5 +21,12 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students
+    SQL
+
+    DB[:conn].execute(sql)
+  end
 
 end
