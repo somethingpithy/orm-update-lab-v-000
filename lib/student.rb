a5746745f8e032c844c837491c042be5aba57bae
Carlota Bindner <cev.bindner@gmail.com>
2018-03-21 14:42:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,4 +29,24 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def save
+
+  end
+
+  def self.create
+
+  end
+  
+  def self.new_from_db
+
+  end
+
+  def self.find_by_name
+
+  end
+
+  def update
+
+  end
+  
 end
