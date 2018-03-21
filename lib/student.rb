5c99a140bbf1103f24574ed4a78bff7309108e2e
Carlota Bindner <cev.bindner@gmail.com>
2018-03-21 15:51:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,8 +43,10 @@ class Student
     end
   end
 
-  def self.create
-
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
 
   def self.new_from_db
